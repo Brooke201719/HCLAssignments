129ca42a0f252c88acd4d1fb3ef40e02b2b6f3ab
Brooke201719 <109536637+Brooke201719@users.noreply.github.com>
2022-08-01 09:33:24 -0500

    Add files via upload

---
+++ AssignmentThree/src/main/java/com/usk/assignment/Test1.java
@@ -0,0 +1,14 @@
+package com.usk.assignment;
+
+public class Test1 {
+	public static void main(String args[]) {
+		
+		System.out.println("I am learning Java!");
+		String s1 = "I am learning";
+		String s2 = "Java!";
+		System.out.println("String length is : "+s1.length());
+		System.out.println("String length is : "+s2.length());
+		
+	}
+
+}
